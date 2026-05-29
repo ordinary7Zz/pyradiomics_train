7aae8196b36734abd267950bd090c91ddd42ddae
instincts <2606391015@qq.com>
2026-05-29 16:05:50 +0800

    init

--- shap_analyze/shap_analyze_autogluon_fixed_Malignancy_Cls.sh
+++ shap_analyze/shap_analyze_autogluon_fixed_Malignancy_Cls.sh
@@ -8,8 +8,8 @@ python shap_analyze/shap_analyze_autogluon_fixed.py \
   --train_csv "$TRAIN_CSV" \
   --label "label" \
   --output_dir "$OUTPUT_DIR" \
-  --main_models LightGBMXT_BAG_L1 \
-  --plot_beeswarm_for LightGBMXT_BAG_L1 \
+  --main_models LightGBM_BAG_L2 \
+  --plot_beeswarm_for LightGBM_BAG_L2 \
   --background_samples 100 \
   --explain_samples 2000 \
   --skip_neural_net \
