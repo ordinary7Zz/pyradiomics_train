a8e99f135a1e9a1bb62a4662fbc27422224b339c
instincts <2606391015@qq.com>
2026-06-02 16:46:12 +0800

    edit

--- shap_analyze/analyze_single_image/analyze_single_image.sh
+++ shap_analyze/analyze_single_image/analyze_single_image.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 MODEL_DIR="./binary_class/outputs/models/BM_dataset3/BM"
 TRAIN_CSV="./binary_class/outputs/task_csvs/BM_dataset3/test_BM.csv"
-TARGET_FILENAME="ThyroidXL_test_00000215_564DFC1D_0.png"
+TARGET_FILENAME="TN3K_test_0040.png"
 OUTPUT_DIR="./binary_class/single_image"
 
 python shap_analyze/analyze_single_image/analyze_single_image.py \
@@ -13,6 +13,6 @@ python shap_analyze/analyze_single_image/analyze_single_image.py \
   --label "label" \
   --output_dir "$OUTPUT_DIR" \
   --main_models LightGBM_BAG_L1 \
-  --background_samples 100 \
+  --background_samples 500 \
   --top_features 5 \
   --skip_neural_net
