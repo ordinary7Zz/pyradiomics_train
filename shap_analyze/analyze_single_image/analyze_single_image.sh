b7b84afb1c454133955a1fb104dbaf4daeb748a7
instincts <2606391015@qq.com>
2026-06-02 10:29:16 +0800

    edit

--- shap_analyze/analyze_single_image/analyze_single_image.sh
+++ shap_analyze/analyze_single_image/analyze_single_image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 MODEL_DIR="./binary_class/outputs/models/BM_dataset3/BM"
 TRAIN_CSV="./binary_class/outputs/task_csvs/BM_dataset3/train_BM.csv"
 TARGET_FILENAME="ThyroidXL_train_00003284_1F60E8DF_3.png"
-OUTPUT_DIR="./binary_class/shap_analysis_outputs/single_image"
+OUTPUT_DIR="./binary_class/single_image"
 
 python shap_analyze/analyze_single_image/analyze_single_image.py \
   --model_dir "$MODEL_DIR" \
