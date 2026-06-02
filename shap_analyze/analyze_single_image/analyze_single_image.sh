80d34730b7f8484e2d0d7347218f4d5774882012
instincts <2606391015@qq.com>
2026-06-02 11:41:20 +0800

    edit

--- shap_analyze/analyze_single_image/analyze_single_image.sh
+++ shap_analyze/analyze_single_image/analyze_single_image.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 MODEL_DIR="./binary_class/outputs/models/BM_dataset3/BM"
-TRAIN_CSV="./binary_class/outputs/task_csvs/BM_dataset3/train_BM.csv"
-TARGET_FILENAME="ThyroidXL_train_00002730_C9690598_1.png"
+TRAIN_CSV="./binary_class/outputs/task_csvs/BM_dataset3/test_BM.csv"
+TARGET_FILENAME="ThyroidXL_test_00000215_564DFC1D_0.png"
 OUTPUT_DIR="./binary_class/single_image"
 
 python shap_analyze/analyze_single_image/analyze_single_image.py \
