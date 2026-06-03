7cb759b209e51fa3cb4256b69324416552acc79d
instincts <2606391015@qq.com>
2026-06-03 16:50:34 +0800

    edit

--- shap_analyze/analyze_single_image/analyze_single_image_FTCPTC.sh
+++ shap_analyze/analyze_single_image/analyze_single_image_FTCPTC.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 MODEL_DIR="./binary_class/outputs/models/FTCPTC_FangDai/FTCPTC"
 TRAIN_CSV="./binary_class/outputs/task_csvs/FTCPTC_FangDai/train_FTCPTC.csv"
-TARGET_FILENAME="FangDai/FTC/B_000103587_20130527_US_1_4.png"
+TARGET_FILENAME="FangDai/PTC/A_b17171017081122.png"
 OUTPUT_DIR="./binary_class/single_image/FTCPTC"
 
 python shap_analyze/analyze_single_image/analyze_single_image.py \
